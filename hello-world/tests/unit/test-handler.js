1303809306a217e5719652128e47ba523899fd8f
moinmulla10 <moinmulla10@gmail.com>
2024-01-31 06:42:56 +0000

    Changed return message

--- hello-world/tests/unit/test-handler.js
+++ hello-world/tests/unit/test-handler.js
@@ -16,7 +16,7 @@ describe('Tests index', function () {
         let response = JSON.parse(result.body);
 
         expect(response).to.be.an('object');
-        expect(response.message).to.be.equal("I am using canary deployments");
+        expect(response.message).to.be.equal("I am using canary deployments, Friendo!");
         // expect(response.location).to.be.an("string");
     });
 });
